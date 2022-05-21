1680c849825a4667333790f9f5fa3a9b5b998e5f
vshyrmanov <vshyrmanov@clearent.com>
2022-05-21 23:01:13 +0200

    added application for az app service

---
+++ Az204.WebApi/Az204.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,31 @@
+﻿using Az204.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Az204.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public FilesController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save([FromForm] FileModel file)
+        {
+            if(file.File == null)
+            {
+                return BadRequest("Please send us a file");
+            }
+
+            string path = Path.Combine(_env.WebRootPath, "docs", Guid.NewGuid().ToString());
+            using var stream = new FileStream(path, FileMode.Create);
+            await file.File.CopyToAsync(stream);
+            return Ok($"Processed doc {file.Title} v{file.Version} - {file.File.FileName}");
+        }
+    }
+}
---
+++ Az204.WebApi/Az204.WebApi/Models/FileModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Az204.WebApi.Models
+{
+    public class FileModel
+    {
+        public string? Title { get; set; }
+
+        public string? Version { get; set; }
+
+        public IFormFile? File { get; set; }
+    }
+}
